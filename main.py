a7705549c5f867166ff8d1d3586ab2138be8fdb0
minhokiller <andresuave.sc@gmail.com>
2023-03-22 17:12:07 -0300

    ajustes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import turtle
 from turtle import Screen
 from pista import Pista
 from jogador import Jogador
@@ -20,6 +21,13 @@ while jogo_on == True:
     time.sleep(0.1)
     carro_config.aparecer_carro()
     carro_config.mover_carro()
+    if jogador.xcor() > 120 or jogador.xcor() < -120:
+        turtle.write(f"Game Over", align='center', font=('arial',40,'bold'))
+        jogo_on = False
+    for carrinho in carro_config.carros:
+        if abs(jogador.xcor() - carrinho.xcor()) <1 and jogador.distance(carrinho) < 65:
+            turtle.write(f"Game Over", align='center', font=('arial',40,'bold'))
+            jogo_on = False
     tela.update()
 tela.exitonclick()
 
