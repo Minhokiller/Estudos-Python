da427e192774e29e09f325a6e0a22b6bc9ef3e18
minhokiller <andresuave.sc@gmail.com>
2023-03-22 15:43:28 -0300

    add carros

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from turtle import Screen
 from pista import Pista
+from jogador import Jogador
+from carro import CarroConfig
 
 tela = Screen()
 tela.setup(600,600)
 tela.tracer(0)
 pista=Pista()
+tela.listen()
+jogador=Jogador()
+carro_config=CarroConfig()
+tela.onkey(jogador.mover_direita,'Right')
+tela.onkey(jogador.mover_esquerda,'Left')
 tela.title('Tarturega')
 jogo_on= True
 while jogo_on == True:
