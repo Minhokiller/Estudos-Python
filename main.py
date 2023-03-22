f3ee81065099c487d51c4aea5667ce91776c4707
minhokiller <andresuave.sc@gmail.com>
2023-03-22 14:40:09 -0300

    enviado arquivo pista

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from turtle import Screen
+from pista import Pista
 
 tela = Screen()
 tela.setup(600,600)
+tela.tracer(0)
+pista=Pista()
 tela.title('Tarturega')
+jogo_on= True
+while jogo_on == True:
+    tela.update()
 tela.exitonclick()
+
---
+++ pista.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+
+class Pista(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.pensize(4)
+        self.penup()
+        self.goto(150,-400)
+        self.pendown()
+        self.goto(150,400)
+
+    #2linha
+        self.desenhar_traco((90,-400))
+        self.desenhar_traco((-90,-400))
+        
+    #3linha
+        self.desenhar_traco((30,-400))
+        self.desenhar_traco((-30,-400))
+
+    #linhafinal
+        self.penup()
+        self.pensize(4)
+        self.goto(-150,-400)
+        self.pendown()
+        self.goto(-150,400)
+
+    def desenhar_traco(self,pos):
+        self.setheading(90)
+        self.penup()
+        self.goto(pos)
+        for traco in range(30):
+            self.forward(20)
+            self.penup()
+            self.forward(20)
+            self.pendown()
